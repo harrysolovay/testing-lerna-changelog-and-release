13b1e8a13aacc09106c7bf86d347371f80818e31
Harry Solovay <harrysolovay@gmail.com>
2021-06-30 13:16:34 -0400

    chore: still debugging

---
+++ .versionrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "package.json",
+      type: "json",
+    },
+    {
+      filename: "lerna.json",
+      type: "json",
+    },
+    {
+      filename: "packages/first/package.json",
+      type: "json",
+    },
+    {
+      filename: "packages/second/package.json",
+      type: "json",
+    },
+  ],
+  types: [
+    {
+      type: "feat",
+      section: "Features",
+    },
+    {
+      type: "fix",
+      section: "Bug Fixes",
+    },
+    {
+      type: "chore",
+    },
+    {
+      type: "docs",
+    },
+    {
+      type: "style",
+      hidden: true,
+    },
+    {
+      type: "refactor",
+      hidden: "Refactoring",
+    },
+    {
+      type: "perf",
+      section: "Performance Enhancements",
+    },
+    {
+      type: "test",
+      hidden: true,
+    },
+  ],
+};
